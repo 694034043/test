0f8a4433f885bd2aaa73fffdc3d6c76c37fb2be3
yenanxing <yenanxing@formssi.com>
2018-04-27 17:23:54 +0800

    1122

--- Test/src/socket/InetAddressTest.java
+++ Test/src/socket/InetAddressTest.java
@@ -11,6 +11,7 @@ public class InetAddressTest {
 		try {
 			InetAddress address = InetAddress.getLocalHost();
 			System.out.println(address.getHostName());
+			System.out.println(address.getHostName());
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
