782721f73e6bd770bcc2d42a1239b51aad81afa4
yenanxing <yenanxing@formssi.com>
2018-04-27 17:16:41 +0800

    yenanxing commit

--- Test/src/reflect/cglib/CglibProxyTest.java
+++ Test/src/reflect/cglib/CglibProxyTest.java
@@ -1,6 +1,6 @@
 package reflect.cglib;
 
-public class CglibProxyTest implements MethodInterceptor {
+public class CglibProxyTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
